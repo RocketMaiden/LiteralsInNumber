898bee5ec6cd0d47ac84b09298c9e40853a354bc
RocketMaiden <elizabeth.ruchko@gmail.com>
2023-07-13 12:01:27 +0300

    sum calculated

--- LiteralsInNumbers/Program.cs
+++ LiteralsInNumbers/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number = 1813;
+int length = 0;
+int[] number_array = new int[10];
+
+/*while(number%10 > 0)
+{
+    number = number % 10;
+    length++;
+}*/
+
+//number_array = new int[length];
+
+while (number % 10 > 0)
+{
+    number_array[number % 10] = number / 10;
+    number %= 10;
+    length++;
+}
+int sum = 0;
+
+/*foreach (int letter in number_array)
+{
+    sum += letter;
+}*/
+
+for (int i = 0; i < length; i++)
+{
+    sum += number_array[i];
+}
